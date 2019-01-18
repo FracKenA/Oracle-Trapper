56347e16f0b770d91c63361ddf6e85bb356b821a
abdhas <33960614+abdhas@users.noreply.github.com>
2019-01-18 09:27:30 +0100

    Update oracle-trapper.lua

--- oracle-trapper.lua
+++ oracle-trapper.lua
@@ -1,84 +1,47 @@
-require(“socket”)
-
-function getHostNameOfAddress(ipaddress)
-
-   local https = require(“ssl.https”)
-   op5server = "localhost"
-   username = "administrator"
-   password = "administrator"
-
-   local body, code, headers, status = https.request("https://"..username..":"..password.."@"..op5server.."/api/filter/query?query=%5Bhosts%5Daddress%3D%22"..ipaddress.."%22&columns=name")
-
-   --log (body)
-   name = string.match(body, “%name\“:\“(%w+)%\“”)
-
-   if (name == nil) then
-      name = "DefaultHostName"
-      log ("Could not get the hostname of ipaddress " .. ipaddress)
-   end
-
-   return name; 
-end
-function encodeURI(str)
-		if (str) then
-			str = string.gsub (str, "\n", "\r\n")
-			str = string.gsub (str, "([^%w ])",
-				function (c) return string.format ("%%%02X", string.byte(c)) end)
-			str = string.gsub (str, " ", "+")
-	   end
-	   return str
-	end
-
-function createServiceIfNotExist(hostname,service)
-	-- body
-	hostname = encodeURI(hostname)
-	service = encodeURI(service)
-
-	url = "https://172.27.105.54/api/status/service/"..hostname..";"+service+""
-	
-	--if 404 create and save config
-end
-
 log ("Trap OID " .. trap.oid)
 
 result.notify = NOTIFY.ALWAYS
 trapType = trap.fields['.1.3.6.1.4.1.111.15.3.1.1.12.1']
 
-if trapType = "Availability" then
-    ipaddress = trap.fields['.1.3.6.1.4.1.111.15.3.1.1.24.1']
+if trapType == "Availability" then
+    fullhostname = trap.fields['.1.3.6.1.4.1.111.15.3.1.1.24.1']
+    --STRING: "SRV02406.vi.corp"      
     state = trap.fields['.1.3.6.1.4.1.111.15.3.1.1.52.1']
-    
+    --STRING: "UP"
     result.message = trap.fields ['.1.3.6.1.4.1.111.15.3.1.1.46.1']
-    typeOfDevice = ["1.3.6.1.4.1.111.15.3.1.1.23.1"]
-    --result.host = getHostNameOfAddress(ipaddress)
-    result.host = "Test Host"
+    --STRING: "The pluggable database is up." 
+    typeOfDevice = trap.fields ['.1.3.6.1.4.1.111.15.3.1.1.23.1']
+    --STRING: "Pluggable Database"        
+    result.host = string.match(fullhostname, "(%w+)%.")
+    --SRV02406
+    oraEM4AlertTargetType = trap.fields['.1.3.6.1.4.1.111.15.3.1.1.21.1']
+    -- STRING: "C02146C_SNMP_SPACEDB"     
 
     if string.match(typeOfDevice, "Database") then
-    	service  = "Oracle "..trap.fields['.1.3.6.1.4.1.111.15.3.1.1.23.1'].." "..trap.fields['.1.3.6.1.4.1.111.15.3.1.1.21.1']
-    	--createServiceIfNotExist(service) -- if 404 on service, create it. else do nothing.
-    	--service = "Oracle Database Instance C02146C_SNMP" OR service = “Oracle Pluggable Database C02146C_SNMP_SPACEDB”
+      log("Device is of type: Database")
+      service  = "Oracle "..typeOfDevice.." "..oraEM4AlertTargetType
     else
-    	service  = "Oracle "..trap.fields['.1.3.6.1.4.1.111.15.3.1.1.23.1']
-    	--service = “Oracle Host”
-        --service = “Oracle Agent”
-        --service = “Oracle Listener”
+      log("Device is not of type: Database")
+      service  = "Oracle "..typeOfDevice
     end
-    trap.service = service
+
+    result.service = service
 
     if string.match(state, "DOWN") or string.match(state, "ERROR")  then
-    	result.state = STATE.CRITICAL
+      log ("State is Critical")
+      result.state = STATE.CRITICAL
 
     elseif string.match(state, "UP") then
-    	result.state = STATE.OK
+      log ("State is UP")
+      result.state = STATE.OK
 
     else
-    	log("Unknown state. Did not receive DOWN, ERROR or UP. Received: " .. state)
-    	result.state = STATE.UNKNOWN
+      log("Unknown state. Did not receive DOWN, ERROR or UP. Received: " .. state)
+      result.state = STATE.UNKNOWN
 
-    end	
-log("Updated Host: " .. result.host .." Service/name: "  .. result.service .. " and state: " .. result.state)
+    end 
+log("Updated Host: " .. result.host .." Service: "  .. result.service .. " and state: " .. result.state)
 
 else
-	log ("Skipping trap type: " .. typeOfTrap)
-end
+  log ("Skipping trap type: " .. trapType)
 end
